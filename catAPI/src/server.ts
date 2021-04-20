35a58599a5d3009c54930b5c22b20cf322090758
RoseMagura <magurarm@guilford.edu>
2021-04-20 16:23:40 -0400

    Edit: Tweak server file

--- catAPI/src/server.ts
+++ catAPI/src/server.ts
@@ -4,6 +4,7 @@ import { createRelationships } from "./initDB";
 const port = process.env.PORT || 3000;
 
 app.listen(port, (): void => {
-  createRelationships();
   console.log(`App listening at http://localhost:${port}`);
+
+  createRelationships();
 });
