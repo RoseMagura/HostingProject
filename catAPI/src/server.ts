3676bc6fd9ee38dae28a513b7a3e1e8cfe4be880
RoseMagura <magurarm@guilford.edu>
2021-04-20 16:34:35 -0400

    Edit: Edit app listen to work with heroku

--- catAPI/src/server.ts
+++ catAPI/src/server.ts
@@ -2,9 +2,9 @@ import app from "./app";
 import { createRelationships } from "./initDB";
 
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
-app.listen(port, (): void => {
-  console.log(`App listening at http://localhost:${port}`);
-
+app.listen(Number(port), host, (): void => {
+  console.log(`App listening at http://${host}:${port}`);
   createRelationships();
 });
